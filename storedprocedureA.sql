a3cfe09920a594ca9c5d9695b468175e4a8e87cc
AlejandraCondeMendez <alejandracondemendez@gmail.com>
2024-08-26 11:33:41 -0600

    creacion de stored procedure, puede agregar una nueva reserva

---
+++ storedprocedureA.sql
@@ -0,0 +1,15 @@
+USE Hoteles;
+
+
+#CREATE TABLE Reserva (codigo_reserva VARCHAR(10) PRIMARY KEY,
+#fecha_llegada DATE,
+#fecha_salida DATE);
+
+DELIMITER //
+CREATE PROCEDURE agregar_nueva_reserva(IN codigo_reserva VARCHAR(10), IN fecha_llegada DATE, IN fecha_salida DATE)
+BEGIN
+    INSERT INTO Reserva (codigo_reserva, fecha_llegada, fecha_salida) VALUES (codigo_reserva, fecha_llegada, fecha_salida);
+END //
+DELIMITER;
+
+CALL agregar_nueva_reserva ('R002', '2024-08-28', '2024-09-08');
