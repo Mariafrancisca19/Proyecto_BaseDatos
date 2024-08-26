f781ee0a00b2f461df4e76f1b32fdb00f74bcbdc
AlejandraCondeMendez <alejandracondemendez@gmail.com>
2024-08-26 11:01:15 -0600

    Estructura sql

--- creacion-bd.sql
+++ creacion-bd.sql
@@ -1,1 +1,12 @@
 CREATE DATABASE Hoteles;
+USE Hoteles;
+
+
+
+
+#Stored Procedures:
+# ○ Crear al menos dos procedimientos almacenados (stored procedures). Ejemplos:
+# ■ Agregar una nueva reserva.
+# ■ Consultar la disponibilidad de habitaciones por fecha.
+# ■ Calcular la ocupación de un hotel en un rango de fechas.
+# ■ Cancelar una reserva existente.
