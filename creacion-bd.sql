470b89e219bd7ec4c6b17d3c1b547a94a84295fa
AlejandraCondeMendez <alejandracondemendez@gmail.com>
2024-08-27 00:02:26 -0600

    creación de PROCEDURES para agregar datos a habitación, pago y clientes, el fin es poder mejorar el código y optimizarlo.

--- creacion-bd.sql
+++ creacion-bd.sql
@@ -8,7 +8,7 @@ DROP TABLE Hotel;
 DROP TABLE Reserva;
 DROP TABLE Pago;
 DROP TABLE Habitacion;
-DROP TABLE Reserva;
+DROP TABLE Codigos;
 
 # Seleccionar tablas
 SELECT * FROM Administrador;
@@ -19,13 +19,21 @@ SELECT * FROM Pago;
 SELECT * FROM Habitacion;
 SELECT * FROM Codigos;
 --  Insertar datos en tablas
-INSERT INTO Administrador (codigo_admi,nombre, apellido,correo) VALUES ("A001","Gabriel","Garcia","garcia@correo.com");
-INSERT INTO Clientes (numero_cedula, nombre, apellido, correo) VALUES (101170698,"Maria","Gonzales","gonza@unn.com");
-INSERT INTO Hotel (razon_social, nombre, ubicacion) VALUES ("H1357","Sheraton","Zona A");
-INSERT INTO Reserva (codigo_reserva, fecha_llegada,fecha_salida, habitacion_id, numero_cedula, codigo_de_pago) VALUES ("R001","2024-11-23","2024-11-26", "H001", 101170698, "P001");
-INSERT INTO Pago (codigo_de_pago,tipo_pago) VALUES ("P001","Efectivo");
-INSERT INTO Habitacion (habitacion_id,num_habitacion,tipo_habitacion) VALUES ("H001", 234,"individual");
-INSERT INTO Codigos (codigo_reserva, codigo_de_pago,numero_cedula,habitacion_id) VALUES ("R001","P001",101170698,"H001");
+INSERT INTO Administrador (codigo_admi,nombre, apellido,correo) 
+VALUES ("A001","Gabriel","Garcia","garcia@correo.com");
+INSERT INTO Clientes (numero_cedula, nombre, apellido, correo) 
+VALUES (101170698,"Maria","Gonzales","gonza@unn.com");
+INSERT INTO Hotel (razon_social, nombre, ubicacion) 
+VALUES ("H1357","Sheraton","Zona A");
+INSERT INTO Reserva (codigo_reserva, fecha_llegada,fecha_salida, habitacion_id, numero_cedula, codigo_de_pago)
+VALUES ("R001","2024-11-23","2024-11-26", "H001", 101170698, "P001");
+INSERT INTO Pago (codigo_de_pago,tipo_pago)
+VALUES ("P001","Efectivo");
+INSERT INTO Habitacion (habitacion_id,num_habitacion,tipo_habitacion)
+VALUES ("H001", 234,"individual");
+INSERT INTO Codigos (codigo_reserva, codigo_de_pago,numero_cedula,habitacion_id)
+VALUES ("R001","P001",101170698,"H001");
+# DUDAAAAAAAAA CON EL PROCEDURE
 
 
 CREATE TABLE Administrador (codigo_admi VARCHAR(10) PRIMARY KEY,
@@ -69,5 +77,6 @@ habitacion_id VARCHAR(10), FOREIGN KEY(habitacion_id) REFERENCES Habitacion(habi
 
  ALTER TABLE habitacion ADD COLUMN disponible BOOLEAN;
 
- SELECT * FROM reserva;
- SELECT * FROM habitacion
+# Este ALTER modifica la columna tipo_habitacion, antes estaba en VARCHAR y ahora en TEXT.
+ ALTER TABLE habitacion MODIFY tipo_habitacion TEXT;
+
--- storedprocedureA.sql
+++ storedprocedureA.sql
@@ -1,24 +1,84 @@
 USE Hoteles;
 
-
-#CREATE TABLE Reserva (codigo_reserva VARCHAR(10) PRIMARY KEY,
-#fecha_llegada DATE,
-#fecha_salida DATE);
-
+# AGREGAR CLIENTES
 DELIMITER //
-CREATE PROCEDURE agregar_nueva_reserva(IN codigo_reserva VARCHAR(10), IN fecha_llegada DATE, IN fecha_salida DATE)
-BEGIN
-    INSERT INTO Reserva (codigo_reserva, fecha_llegada, fecha_salida) VALUES (codigo_reserva, fecha_llegada, fecha_salida);
+CREATE PROCEDURE agregar_clientes(IN numero_cedula INT, 
+IN nombre VARCHAR(60), 
+IN apellido VARCHAR(60), 
+IN correo VARCHAR(60))
+BEGIN 
+    INSERT INTO Clientes (numero_cedula, nombre, apellido, correo) 
+    VALUES (numero_cedula, nombre, apellido, correo);
 END //
 DELIMITER;
 
-CALL agregar_nueva_reserva ('R002', '2024-08-28', '2024-09-08');
-CALL agregar_nueva_reserva ('R003', '2024-05-15', '2024-05-20');
+SELECT * FROM Clientes;
+CALL agregar_clientes(202304586, 'Juan', 'Perez', 'perez@unn.com');
+CALL agregar_clientes(234567890, 'Ana', 'Garcia', 'anag@unn.com');
+CALL agregar_clientes(345678901, 'Carlos', 'Rodriguez', 'rodriguez@unn.com');
+CALL agregar_clientes(456789012, 'Maria', 'Lopez', 'lopez@unn.com');
 
-
+# AGREGAR HABITACIONES
 DELIMITER //
-CREATE PROCEDURE cancelar_reserva(IN codigo_reserva VARCHAR(10), IN fecha_llegada DATE, IN fecha_salida DATE)
+CREATE PROCEDURE agregar_habitaciones (IN habitacion_id VARCHAR(10), 
+IN num_habitacion INT, 
+IN tipo_habitacion TEXT)
 BEGIN
-    DELETE FROM Reserva WHERE
+    INSERT INTO Habitacion (habitacion_id, num_habitacion, tipo_habitacion) 
+    VALUES (habitacion_id, num_habitacion, tipo_habitacion);
 END //
 DELIMITER;
+
+CALL agregar_habitaciones('H002', 507, 'VIP');
+CALL agregar_habitaciones('H003', 608, 'matrimonial');
+SELECT * FROM Habitacion;
+DROP PROCEDURE agregar_habitaciones;
+ALTER TABLE habitacion ALTER disponible SET DEFAULT 0;
+
+# AGREGAR PAGO
+DELIMITER //
+CREATE PROCEDURE agregar_pago(IN codigo_de_pago VARCHAR(10), tipo_pago VARCHAR(10))
+BEGIN
+    INSERT INTO Pago (codigo_de_pago, tipo_pago) 
+    VALUES (codigo_de_pago, tipo_pago);
+END //
+DELIMITER;
+CALL agregar_pago ('P002', 'Plazos');
+CALL agregar_pago ('P003', 'Contado');
+CALL agregar_pago ('P004', 'Plazos');
+SELECT * FROM Pago;
+DROP PROCEDURE agregar_pago;
+
+
+# AGREGAR RESERVA
+DROP PROCEDURE agregar_nueva_reserva;
+DELIMITER //
+CREATE PROCEDURE agregar_nueva_reserva(IN codigo_reserva VARCHAR(10), 
+IN fecha_llegada DATE, 
+IN fecha_salida DATE, 
+IN habitacion_id VARCHAR(10),
+IN numero_cedula INT,
+IN codigo_de_pago VARCHAR(10))
+BEGIN
+    INSERT INTO Reserva (codigo_reserva, fecha_llegada, fecha_salida, habitacion_id, numero_cedula, codigo_de_pago) 
+    VALUES (codigo_reserva, fecha_llegada, fecha_salida, habitacion_id, numero_cedula, codigo_de_pago);
+END //
+DELIMITER;
+
+CALL agregar_nueva_reserva ('R002', '2024-06-12', '2024-06-15', 'H003', 234567890, 'P003');
+
+SELECT * FROM reserva;
+
+UPDATE habitacion SET disponible = 1 WHERE habitacion_id = "H001";
+# ES NECESARIO? YA SE CREO EL ALTER TABLE QUE AGREGA POR DEFECTO EL 1 y 0
+
+
+# PROCEDURE QUE ELIMINA RESERVAS SEGÚN EL CÓDIGO DE RESERVA
+DELIMITER //
+CREATE PROCEDURE cancelar_reserva(IN e_codigo_reserva VARCHAR(10))
+BEGIN
+    DELETE FROM Reserva WHERE codigo_reserva = e_codigo_reserva;
+END //
+DELIMITER;
+CALL cancelar_reserva('R001');
+SELECT * FROM Reserva;
