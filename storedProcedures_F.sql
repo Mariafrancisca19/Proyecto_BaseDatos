5f741fc831e9fcc7aecf1f8408749d46cdfe19f7
AlejandraCondeMendez <alejandracondemendez@gmail.com>
2024-08-30 09:14:45 -0600

    TRIGGERS STORED PROCEDURES

--- storedProcedures_F.sql
+++ storedProcedures_F.sql
@@ -3,19 +3,28 @@ USE Hoteles;
 DELIMITER //
 CREATE PROCEDURE disponibilidad_habitacion (IN disponibilidad_fecha DATE)
 BEGIN  
-    SELECT Hotel.nombre, Habitacion.num_habitacion, Habitacion.disponible, Reserva.fecha_salida 
-    FROM habitacion 
-    INNER JOIN Hotel ON Hotel.razon_social = Habitacion.razon_social 
-    RIGHT JOIN Reserva ON Reserva.razon_social = Habitacion.razon_social
-    AND Habitacion.num_habitacion = Reserva.habitacion_id
-    AND Reserva.fecha_salida >= disponibilidad_fecha
+    SELECT Hotel.nombre, Habitacion.habitacion_id, Habitacion.disponible, Reserva.fecha_salida 
+    FROM Habitacion 
+    INNER JOIN Hotel 
+    ON Hotel.razon_social = Habitacion.razon_social
+    RIGHT JOIN Reserva 
+    ON Reserva.razon_social = Habitacion.razon_social
+    AND Habitacion.habitacion_id = Reserva.habitacion_id
+    AND Reserva.fecha_salida <= disponibilidad_fecha
     WHERE Reserva.fecha_salida < disponibilidad_fecha;
 END //
 DELIMITER;
-CALL disponibilidad_habitacion("2024-12-1");
+CALL disponibilidad_habitacion("2024-05-30");
 DROP PROCEDURE disponibilidad_habitacion;
 
 
 SELECT * FROM Reserva;
 SELECT * FROM Habitacion;
 SELECT * FROM reserva;
+SELECT * FROM hotel;
+SELECT Hotel.nombre, Habitacion.habitacion_id, Habitacion.disponible, Reserva.fecha_salida 
+FROM Habitacion 
+INNER JOIN Hotel
+ON Hotel.razon_social = Habitacion.razon_social
+RIGHT JOIN Reserva 
+ON Reserva.razon_social = Habitacion.razon_social;
--- triggers.sql
+++ triggers.sql
@@ -1,5 +1,4 @@
 USE Hoteles;
- 
 # TRIGGER, se creó para que cada ve que se inserte una nueva reserva en la tabla Reserva se notifique y se inserte
 # en la tabla Notificaciones.
 DELIMITER //
@@ -23,10 +22,12 @@ BEGIN
 END //
 DELIMITER ;
 
+
+DROP TRIGGER actualizaion_disponibilidad_habitacion;
 SELECT * FROM habitacion;
 SELECT * FROM reserva
 
 -- Comprobacion de la actualizacion de la disponibilidad de habitacion
 INSERT INTO reserva (codigo_reserva, fecha_llegada, fecha_salida, habitacion_id, id_usuario,codigo_de_pago, razon_social) 
-VALUES ("R030", "2024-06-28", "2024-06-30", "H030", "Ale23", "P001", "RS005");
+VALUES ("R031", "2024-06-28", "2024-06-30", "H028", "Ale23", "P001", "RS005");
 
